81ebed9af363edf76eedec97a015660455787966
Lukas Petersen <lukas.petersen93@yahoo.de>
2020-11-28 16:50:16 +0100

    change up motion

--- snake.py
+++ snake.py
@@ -11,9 +11,9 @@ black=(0,0,0)
 #Screen settings
 WIDTH,HEIGHT=800,600
 SIZE=25
-screen = pygame.display.set_mode((WIDTH,HEIGHT))
+screen = pygame.display.set_mode((WIDTH+10,HEIGHT+10))
 screen.fill(black)
-gamearea = pygame.Surface((WIDTH-10,HEIGHT-10))
+gamearea = pygame.Surface((WIDTH,HEIGHT))
 pygame.display.set_caption("Snake")
 #icon = pygame.image.load('ship.png')
 #pygame.display.set_icon(icon)
@@ -47,80 +47,108 @@ class Object():
     def collide(self, other):
         delta_x = int(self.x-other.x)
         delta_y = int(self.y-other.y)
-        return self.mask.overlap(other.mask,(delta_x,delta_y)) != None
+        rect_1=pygame.Rect(self.x,self.y,SIZE,SIZE)
+        rect_2=pygame.Rect(other.x,other.y,SIZE,SIZE)
+        return rect_1.colliderect(rect_2)
 
 class Snake(Object):
     img = pygame.image.load('pixel.png')
     img = pygame.transform.scale(img, (SIZE, SIZE))
-    vel = SIZE
+    vel = 3
     x_vel = vel
     y_vel = 0
+    progress_x = 0
+    progress_y = 0
     def __init__(self,x,y):
         super().__init__(x,y)
+        self.show_x=x
+        self.show_y=y
         self.next=None
 
     def eat(self, food):
-        distance=((self.x-food.x)**2+(self.y-food.y)**2)
-        if distance<SIZE:
+        if self.collide(food):
             food.respawn()
             return True
         return False
 
-    @pixelate
     def move(self, food):
         def pull_tail(snake_body):
             if snake_body.next:
                 pull_tail(snake_body.next)
                 snake_body.next.x, snake_body.next.y = snake_body.x, snake_body.y
 
-            elif grow:
+            elif growth:
                 snake_body.next=(Snake(snake_body.x,snake_body.y))
+
+            snake_body.show_x=snake_body.x//SIZE*SIZE
+            snake_body.show_y=snake_body.y//SIZE*SIZE
+
+        progress_x = self.x % SIZE
+        progress_y = self.y % SIZE
         
-        grow=self.eat(food)
-        if self.next:
-            pull_tail(self.next)
-            self.next.x,self.next.y=self.x,self.y
-        elif grow:
-            self.next=Snake(self.x,self.y)
+        if self.x_vel>0 or self.y_vel>0:
+            if progress_x < self.progress_x or progress_y < self.progress_y:
+                growth=self.eat(food)
+                pull_tail(self)
+                Snake.progress_x = 0
+                Snake.progress_y = 0
+        
+        elif self.x_vel<0 or self.y_vel<0:
+            if progress_x > self.progress_x or progress_y > self.progress_y:
+                growth=self.eat(food)
+                pull_tail(self)
+                Snake.progress_x = SIZE
+                Snake.progress_y = SIZE
+
+        else:
+            Snake.progress_x = progress_x
+            Snake.progress_y = progress_y
         
         self.x += self.x_vel
         self.y += self.y_vel
+
+        print((food.x,food.y),(self.x,self.y))
     
     def show(self):
-        gamearea.blit(self.img,(self.x,self.y))
+        gamearea.blit(self.img,(self.show_x,self.show_y))
         if self.next:
             self.next.show()
+
+
 
 class Food(Object):
     img = pygame.image.load('pixel.png')
     img = pygame.transform.scale(img, (SIZE, SIZE))
+    border_x=range(0,WIDTH,SIZE)
+    border_y=range(0,HEIGHT,SIZE)
+
+    def __init__(self):
+        self.x=random.choice(self.border_x)
+        self.y=random.choice(self.border_y)
 
     def respawn(self):
-        self.x=random.choice(range(0,WIDTH,SIZE))
-        self.y=random.choice(range(0,HEIGHT,SIZE))
+        self.x=random.choice(self.border_x)
+        self.y=random.choice(self.border_y)
 
 
 def game_over(head):
-    if head.x<0 or head.y<0:
+    if head.x<0 or head.y<0 or head.x>WIDTH or head.y>HEIGHT:
         print("game over")
         return False
     
-    if head.next:
-        body=head.next
-        while True:
-            if head.x==body.x or head.y==body.y:
-                print("game over")
-                return False
+    # if head.next:
+    #     body=head.next
+    #     while True:
+    #         if head.x==body.x or head.y==body.y:
+    #             print("game over")
+    #             return False
             
-            if body.next:
-                body=body.next
-            else:
-                break
+    #         if body.next:
+    #             body=body.next
+    #         else:
+    #             break
     
     return True
-
-
-
 
 def main_menu():
     menu_font=pygame.font.SysFont('impact',40)
@@ -143,11 +171,11 @@ def main_menu():
 
 def play_snake():
     #set Clock
-    FPS = 15
+    FPS = 30
     clock = pygame.time.Clock()
 
-    head=Snake(50,50)
-    food=Food(100,100)
+    head=Snake(int(WIDTH/2),int(HEIGHT/2))
+    food=Food()
 
     running = True
     while running:
