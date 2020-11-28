341a680237a149998be3cadc314f741c1bb35e69
Lukas Petersen <lukas.petersen93@yahoo.de>
2020-11-28 17:58:58 +0100

    fix motion, acceleration, gameover, highscore

--- snake.py
+++ snake.py
@@ -4,6 +4,10 @@ import random
 #Initilaze
 pygame.font.init()
 
+#Highscore
+with open('highscore.txt','r') as f:
+    highscore=f.readline()
+
 #colors
 white=(240,240,240)
 black=(0,0,0)
@@ -54,20 +58,21 @@ class Object():
 class Snake(Object):
     img = pygame.image.load('pixel.png')
     img = pygame.transform.scale(img, (SIZE, SIZE))
-    vel = 3
+    vel = SIZE
     x_vel = vel
     y_vel = 0
-    progress_x = 0
-    progress_y = 0
+    progress = 0
+    fill_progress = 15
+    score = 0
     def __init__(self,x,y):
         super().__init__(x,y)
-        self.show_x=x
-        self.show_y=y
         self.next=None
 
     def eat(self, food):
         if self.collide(food):
             food.respawn()
+            Snake.fill_progress += 2.5
+            Snake.score += 1
             return True
         return False
 
@@ -80,41 +85,29 @@ class Snake(Object):
             elif growth:
                 snake_body.next=(Snake(snake_body.x,snake_body.y))
 
-            snake_body.show_x=snake_body.x//SIZE*SIZE
-            snake_body.show_y=snake_body.y//SIZE*SIZE
+        Snake.progress += Snake.fill_progress
 
-        progress_x = self.x % SIZE
-        progress_y = self.y % SIZE
-        
-        if self.x_vel>0 or self.y_vel>0:
-            if progress_x < self.progress_x or progress_y < self.progress_y:
-                growth=self.eat(food)
-                pull_tail(self)
-                Snake.progress_x = 0
-                Snake.progress_y = 0
-        
-        elif self.x_vel<0 or self.y_vel<0:
-            if progress_x > self.progress_x or progress_y > self.progress_y:
-                growth=self.eat(food)
-                pull_tail(self)
-                Snake.progress_x = SIZE
-                Snake.progress_y = SIZE
+        if Snake.progress > 100:
+            growth=self.eat(food)
+            pull_tail(self)
+            Snake.progress = Snake.progress%100
 
-        else:
-            Snake.progress_x = progress_x
-            Snake.progress_y = progress_y
-        
-        self.x += self.x_vel
-        self.y += self.y_vel
+            self.x += self.x_vel
+            self.y += self.y_vel
 
-        print((food.x,food.y),(self.x,self.y))
-    
+    @classmethod
+    def spawn(cls, start_x, start_y):
+        head = Snake(start_x,start_y)
+        head.next = Snake(start_x-SIZE,start_y)
+        head.next.next = Snake(start_x-SIZE*2,start_y)
+        head.next.next.next = Snake(start_x-SIZE*3,start_y)
+        head.next.next.next.next = Snake(start_x-SIZE*4,start_y)
+        return head
+
     def show(self):
-        gamearea.blit(self.img,(self.show_x,self.show_y))
+        gamearea.blit(self.img,(self.x,self.y))
         if self.next:
             self.next.show()
-
-
 
 class Food(Object):
     img = pygame.image.load('pixel.png')
@@ -132,22 +125,49 @@ class Food(Object):
 
 
 def game_over(head):
-    if head.x<0 or head.y<0 or head.x>WIDTH or head.y>HEIGHT:
-        print("game over")
+    def show_game_over():
+            game_over_font=pygame.font.SysFont('impact',70)
+            score_font=pygame.font.SysFont('impact',30)
+
+            game_over_label=game_over_font.render("GAME OVER",True,(0,0,0))
+            score_label=score_font.render("Score: "+str(Snake.score),True,(0,0,0))
+            highscore_label=score_font.render("Highscore: "+str(highscore),True,(0,0,0))
+
+
+            #update highscore
+            if Snake.score>int(highscore):
+                with open('highscore.txt','w') as f:
+                    try:
+                        f.write(str(Snake.score))
+                    except:
+                        pass
+            x=WIDTH/2
+            y=0
+            while y<=250:
+                y+=1
+                gamearea.fill(white)
+                gamearea.blit(game_over_label,(x-game_over_label.get_width()/2,y))
+                screen.blit(gamearea,(5,5))
+                pygame.time.wait(4)
+                pygame.display.update()
+
+            screen.blit(score_label,(x-score_label.get_width()/2,y+80))
+            screen.blit(highscore_label,(x-highscore_label.get_width()/2,y+120))
+            pygame.display.update()
+            pygame.time.wait(3000)
+    
+    if head.x<0 or head.y<0 or head.x+SIZE>WIDTH or head.y+SIZE>HEIGHT:
+        show_game_over()
         return False
     
-    # if head.next:
-    #     body=head.next
-    #     while True:
-    #         if head.x==body.x or head.y==body.y:
-    #             print("game over")
-    #             return False
-            
-    #         if body.next:
-    #             body=body.next
-    #         else:
-    #             break
-    
+    if head.next:
+        body=head.next
+        while body:
+            if head.collide(body):
+                show_game_over()
+                return False
+            body=body.next
+
     return True
 
 def main_menu():
@@ -174,13 +194,20 @@ def play_snake():
     FPS = 30
     clock = pygame.time.Clock()
 
-    head=Snake(int(WIDTH/2),int(HEIGHT/2))
+    #font
+    highscore_font=pygame.font.SysFont('forte',24)
+    
+    Snake.fill_progress = 15
+    Snake.score = 0
+    start_x, start_y = int(WIDTH/2),int(HEIGHT/2)
+    head=Snake.spawn(start_x, start_y)
     food=Food()
 
     running = True
     while running:
         clock.tick(FPS)
-
+        score=highscore_font.render("Score: "+str(Snake.score),True,(0,0,0))
+        
         #Keyyboard Interaction
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -207,6 +234,7 @@ def play_snake():
         head.show()
         food.show()
         head.move(food)
+        gamearea.blit(score,(WIDTH-10-score.get_width(),10))
         screen.blit(gamearea,(5,5))
         pygame.display.update()
 
